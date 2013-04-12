4c899086b7f9f3942d46dda5567bf022db948c7e
Torsten Irländer <torsten@irlaender.de>
2013-04-13 00:24:28 +0200

    Added new class for mixins of the models. First to mixins are used to defined ownership and information on creation and updates.

---
+++ ringo/model/mixins.py
@@ -0,0 +1,38 @@
+from sqlalchemy.ext.declarative import declared_attr
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+)
+
+from sqlalchemy.orm import (
+    relationship,
+)
+
+
+class Meta(object):
+    created = Column(DateTime)
+    updated = Column(DateTime)
+
+
+class Owned(object):
+
+    @declared_attr
+    def uid(cls):
+        return Column(Integer, ForeignKey('users.id'))
+
+    @declared_attr
+    def user(cls):
+        return relationship("User",
+                            primaryjoin="User.id==%s.uid" % cls.__name__)
+
+    @declared_attr
+    def gid(cls):
+        return Column(Integer, ForeignKey('usergroups.id'))
+
+    @declared_attr
+    def group(cls):
+        return relationship("Usergroup",
+                            primaryjoin="Usergroup.id==%s.gid" % cls.__name__)
