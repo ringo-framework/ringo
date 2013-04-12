fa8474013d6b89a19de91fcb7b7010e21c571853
Torsten Irländer <torsten@irlaender.de>
2013-04-13 00:35:01 +0200

    Removed reference to the meta item

--- ringo/model/modul.py
+++ ringo/model/modul.py
@@ -1,7 +1,6 @@
 import sqlalchemy as sa
 from ringo.model import Base
 from ringo.model.user import BaseItem
-from ringo.model.meta import MetaItem
 from ringo.lib.i18n import _
 
 
@@ -9,17 +8,12 @@ class ModulItem(BaseItem, Base):
     __tablename__ = 'modules'
     _modul_id = 1
     id = sa.Column(sa.Integer, primary_key=True)
-    mid = sa.Column(sa.Integer, sa.ForeignKey('meta.id'))
     name = sa.Column(sa.Text, unique=True, nullable=False)
     label = sa.Column(sa.Text, nullable=False)
     label_plural = sa.Column(sa.Text, nullable=False)
     description = sa.Column(sa.Text)
     str_repr = sa.Column(sa.Text)
 
-    # Relations
-    meta = sa.orm.relation("MetaItem", cascade="all, delete-orphan",
-                           single_parent=True)
-    # Configuration
     _table_fields = [('name', 'Name')]
 
     def __unicode__(self):
@@ -42,36 +36,23 @@ def init_model(dbsession):
     modul = ModulItem(name='modules')
     modul.label = _("Modul")
     modul.label_plural = _("Modules")
-    meta = MetaItem(mid=1, uid=1, gid=None)
-    modul.meta = meta
-    dbsession.add(modul)
     modul = ModulItem(name='users')
     modul.label = _("User")
     modul.label_plural = _("Users")
-    meta = MetaItem(mid=1, uid=1, gid=None)
-    modul.meta = meta
     dbsession.add(modul)
     modul = ModulItem(name='usergroups')
     modul.label = _("Usergroup")
     modul.label_plural = _("Usergroups")
-    meta = MetaItem(mid=1, uid=1, gid=None)
-    modul.meta = meta
     dbsession.add(modul)
     modul = ModulItem(name='roles')
     modul.label = _("Role")
     modul.label_plural = _("Roles")
-    meta = MetaItem(mid=1, uid=1, gid=None)
-    modul.meta = meta
     dbsession.add(modul)
     modul = ModulItem(name='permissions')
     modul.label = _("Permission")
     modul.label_plural = _("Permissions")
-    meta = MetaItem(mid=1, uid=1, gid=None)
-    modul.meta = meta
     dbsession.add(modul)
     modul = ModulItem(name='profiles')
     modul.label = _("Profile")
     modul.label_plural = _("Profiles")
-    meta = MetaItem(mid=1, uid=1, gid=None)
-    modul.meta = meta
     dbsession.add(modul)
