47bbe87458aacef976b81fe19dbf14ca30b74274
Torsten Irländer <torsten.irlaender@intevation.de>
2013-06-20 08:32:41 +0200

    Added functional tests for profiles.

---
+++ ringo/tests/functional/test_profiles.py
@@ -0,0 +1,38 @@
+import unittest
+from sqlalchemy.orm.exc import NoResultFound
+from ringo.model.user import Profile
+from ringo.tests import BaseFunctionalTest
+
+class TestProfile(BaseFunctionalTest):
+
+    def test_wrong_url(self):
+        self.login('admin', 'secret')
+        self.app.get('/profiles/listtest', status=404)
+
+    def test_list(self):
+        self.login('admin', 'secret')
+        self.app.get('/profiles/list', status=200)
+
+    def test_read(self):
+        self.login('admin', 'secret')
+        self.app.get('/profiles/read/1', status=200)
+
+    def test_read_unknown(self):
+        self.login('admin', 'secret')
+        try:
+            self.app.get('/profiles/read/1234')
+        except NoResultFound:
+            return True
+        # Must fail!
+        self.assertEqual(0,1)
+
+    def test_edit(self):
+        self.login('admin', 'secret')
+        self.app.get('/profiles/update/1', status=200)
+
+    def test_edit_save(self):
+        self.login('admin', 'secret')
+        self.app.post('/profiles/update/1', status=200)
+
+if __name__ == '__main__':
+    unittest.main()
