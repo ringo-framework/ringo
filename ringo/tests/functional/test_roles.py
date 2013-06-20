4397a95c629fa84c601d849317e10cc9ac2ea182
Torsten Irländer <torsten.irlaender@intevation.de>
2013-06-20 08:12:36 +0200

    Added functional tests for roles.

---
+++ ringo/tests/functional/test_roles.py
@@ -0,0 +1,75 @@
+import unittest
+from sqlalchemy.orm.exc import NoResultFound
+from ringo.model.user import Role
+from ringo.tests import BaseFunctionalTest
+
+class TestRole(BaseFunctionalTest):
+
+    def test_wrong_url(self):
+        self.login('admin', 'secret')
+        self.app.get('/roles/listtest', status=404)
+
+    def test_list(self):
+        self.login('admin', 'secret')
+        self.app.get('/roles/list', status=200)
+
+    def test_read(self):
+        self.login('admin', 'secret')
+        self.app.get('/roles/read/1', status=200)
+
+    def test_read_unknown(self):
+        self.login('admin', 'secret')
+        try:
+            self.app.get('/roles/read/1234')
+        except NoResultFound:
+            return True
+        # Must fail!
+        self.assertEqual(0,1)
+
+    def test_create(self):
+        self.login('admin', 'secret')
+        self.app.get('/roles/create', status=200)
+
+    def test_create_save_fail(self):
+        self.login('admin', 'secret')
+        # fail because name must not be none!
+        values = {
+            'name': ''
+        }
+        self.app.post('/roles/create', values, status=200)
+
+    def test_create_save_success(self):
+        self.login('admin', 'secret')
+        values = {
+            'name': 'testusergroup'
+        }
+        self.app.post('/roles/create', values, status=302)
+
+    def test_edit(self):
+        self.login('admin', 'secret')
+        self.app.get('/roles/update/1', status=200)
+
+    def test_edit_save(self):
+        self.login('admin', 'secret')
+        self.app.post('/roles/update/1', status=200)
+
+    def test_delete(self):
+        last_id = self.get_max_id(Role)
+        self.login('admin', 'secret')
+        self.app.get('/roles/delete/%s' % last_id, status=200)
+
+    def test_delete_confirmed(self):
+        last_id = self.get_max_id(Role)
+        self.login('admin', 'secret')
+        values = {'confirmed': '1'}
+        self.app.post('/roles/delete/%s' % last_id, values, status=302)
+
+    def test_delete_fail(self):
+        self.test_create_save_success()
+        last_id = self.get_max_id(Role)
+        values = {'confirmed': '0'}
+        self.app.post('/roles/delete/%s' % last_id, values, status=200)
+        self.test_delete_confirmed()
+
+if __name__ == '__main__':
+    unittest.main()
