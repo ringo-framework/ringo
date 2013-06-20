dd0384ce40b9839576b380a35a88332f946013b4
Torsten Irländer <torsten.irlaender@intevation.de>
2013-06-20 09:00:02 +0200

    Added ignore option to the create_default_actions method to ignore certain actions.

--- ringo/model/modul.py
+++ ringo/model/modul.py
@@ -51,18 +51,28 @@ class ModulItem(BaseItem, Base):
         return self.label
 
 
-def _create_default_actions(dbsession):
+def _create_default_actions(dbsession, ignore=[]):
     a0 = ActionItem(name="List", url="list", icon="icon-list-alt")
     a1 = ActionItem(name="Create", url="create", icon=" icon-plus")
     a2 = ActionItem(name="Read", url="read/{id}", icon="icon-eye-open")
     a3 = ActionItem(name="Update", url="update/{id}", icon="icon-edit")
     a4 = ActionItem(name="Delete", url="delete/{id}", icon="icon-trash")
-    dbsession.add(a0)
-    dbsession.add(a1)
-    dbsession.add(a2)
-    dbsession.add(a3)
-    dbsession.add(a4)
-    actions = [a0, a1, a2, a3, a4]
+    actions = []
+    if not "list" in ignore:
+        dbsession.add(a0)
+        actions.append(a0)
+    if not "create" in ignore:
+        dbsession.add(a1)
+        actions.append(a1)
+    if not "read" in ignore:
+        dbsession.add(a2)
+        actions.append(a2)
+    if not "update" in ignore:
+        dbsession.add(a3)
+        actions.append(a3)
+    if not "delete" in ignore:
+        dbsession.add(a4)
+        actions.append(a4)
     return actions
 
 
