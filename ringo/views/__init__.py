0af4fb6fde51b0aef2ce3594f061bd9df6667103
Torsten Irländer <torsten@irlaender.de>
2013-06-25 00:39:26 +0200

    Added helper class to add items into the history

---
+++ ringo/views/__init__.py
@@ -0,0 +1,9 @@
+from ringo.lib.history import History
+
+
+def handle_history(request):
+    history = request.session.get('history')
+    if history is None:
+        history = History()
+    history.push(request.url)
+    request.session['history'] = history
