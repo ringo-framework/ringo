800d51c8a6d7f1a98a8c9da8ef1cebd31da56c53
Torsten Irländer <torsten@irlaender.de>
2013-07-04 01:19:01 +0200

    Hide the savequery model dialog on default.

--- ringo/static/js/init.js
+++ ringo/static/js/init.js
@@ -1,4 +1,7 @@
 $( document ).ready(function() {
-	$('.modal').modal();
-	$('.alert').delay(3 * 1000).fadeOut()
+    $('.modal').modal();
+    $('.alert').delay(3 * 1000).fadeOut()
+    $('#savequerymodal').modal({
+      show: false;
+    })
 });
