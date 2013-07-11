e152cf80edac53ba0a1946b3e9c3161230a18fca
Torsten Irländer <torsten@irlaender.de>
2013-07-11 23:01:46 +0200

    Set version to 0.3.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requires = [
 ]
 
 setup(name='ringo',
-      version='0.3.1',
+      version='0.3.2',
       description='A simple web framework with base functionality to build web applications.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
@@ -68,8 +68,3 @@ setup(name='ringo',
             ('views/**.json', 'tableconfig', None),
             ('static/**', 'ignore', None)]},
       )
-
-
-
-
-
