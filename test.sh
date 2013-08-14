474286c639edcc602b91fe5bf8e102919731fd1e
Torsten Irländer <torsten@irlaender.de>
2013-08-14 21:56:56 +0200

    Show coverage report after the test

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@ rm test.sqlite
 initialize_ringo_db test.ini
 #nosetests
 behave ringo/tests/behave/features --logging-level=ERROR
+coverage report -m --include="ringo*" --omit="ringo/test*"
 rm -r test-data
 rm test.sqlite
