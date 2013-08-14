4ee39adfe310e70ba9141259665930c57d524107
Torsten Irländer <torsten@irlaender.de>
2013-08-14 21:55:55 +0200

    Start coverage on behave tests.

--- ringo/tests/behave/features/environment.py
+++ ringo/tests/behave/features/environment.py
@@ -1,4 +1,5 @@
 import os
+import coverage
 from paste.deploy.loadwsgi import appconfig
 from sqlalchemy import engine_from_config
 from sqlalchemy.orm import sessionmaker
@@ -6,12 +7,15 @@ from sqlalchemy.orm import sessionmaker
 from ringo import main
 from webtest import TestApp
 
+cov = coverage.coverage()
+
 def get_settings():
     here = os.path.dirname(__file__)
     settings = appconfig('config:' + os.path.join(here, '../../../../', 'test.ini'))
     return settings
 
 def before_all(context):
+    cov.start()
     context.engine = engine_from_config(get_settings(), prefix='sqlalchemy.')
     context.Session = sessionmaker()
 
@@ -20,4 +24,6 @@ def before_feature(context, feature):
     context.app = TestApp(main({}, **settings))
 
 def after_all(context):
-    pass
+    cov.stop()
+    cov.save()
+    #cov.html_report()
