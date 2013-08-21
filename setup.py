8aa5cc5195c2c409926903c02d6dc70bbd2ce051
Torsten Irländer <torsten.irlaender@intevation.de>
2013-08-21 09:04:19 +0200

    Set version number to 0.6.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requires = [
 ]
 
 setup(name='ringo',
-      version='0.6.0',
+      version='0.6.1',
       description='A simple web framework with base functionality to build web applications.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
