600718cc32e55474ef4d7ec4fcadd222d6dda207
Torsten Irländer <torsten.irlaender@intevation.de>
2013-09-20 10:29:12 +0200

    Added a readonly version of the ownership form.

--- ringo/views/base.py
+++ ringo/views/base.py
@@ -66,7 +66,10 @@ def render_ownership_form(item, request, readonly=True):
     when rendered in the read form.
     """
     config = Config(load(get_path_to_form_config('ownership.xml', 'ringo')))
-    form_config = config.get_form('ownership-form')
+    if readonly:
+        form_config = config.get_form('ownership-form-read')
+    else:
+        form_config = config.get_form('ownership-form-update')
     form = Form(form_config, item, request.db)
     return form.render()
 
