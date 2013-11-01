6413e7d538ce5c269c3ff9231f271aaf94fe2741
Torsten Irländer <torsten@irlaender.de>
2013-11-01 07:37:09 +0100

    Added function body to change the users password. Might be moved into another view.

--- ringo/views/users.py
+++ ringo/views/users.py
@@ -1,8 +1,15 @@
 import logging
 import hashlib
+import sqlalchemy as sa
+from pyramid.httpexceptions import HTTPBadRequest
 from pyramid.view import view_config
+from formbar.form import Form
 
-from ringo.views.base import list_, create_, update_, read_, delete_
+from ringo.views.base import (list_, create_, update_, read_, delete_,
+                              handle_history, handle_params,
+                              get_current_form_page)
+from ringo.model.mixins import Owned
+
 from ringo.views.json import (
     list_   as json_list,
     create_ as json_create,
@@ -29,6 +36,7 @@ def encrypt_password(request, user):
 ###########################################################################
 #                               HTML VIEWS                                #
 ###########################################################################
+
 
 @view_config(route_name=User.get_action_routename('list'),
              renderer='/default/list.mako',
@@ -63,6 +71,42 @@ def read(request):
              permission='delete')
 def delete(request):
     return delete_(User, request)
+
+
+@view_config(route_name=User.get_action_routename('changepassword'),
+             renderer='/users/changepassword.mako',
+             permission='read')
+def changepassword(request):
+    """Method to change the users password by the user. The user user
+    musst provide his old and the new pasword"""
+    clazz = User
+    handle_history(request)
+    handle_params(clazz, request)
+    _ = request.translate
+    rvalue = {}
+
+    # Load the item return 400 if the item can not be found.
+    id = request.matchdict.get('id')
+    factory = clazz.get_item_factory()
+    try:
+        item = factory.load(id, request.db)
+    except sa.orm.exc.NoResultFound:
+        raise HTTPBadRequest()
+
+    item_form = Form(item.get_form_config('changepassword'), item, request.db, translate=_,
+                renderers=renderers,
+                change_page_callback={'url': 'set_current_form_page',
+                                      'item': clazz.__tablename__,
+                                      'itemid': id},
+                request=request, csrf_token=request.session.get_csrf_token())
+
+    if request.POST:
+        pass
+
+    rvalue['clazz'] = clazz
+    rvalue['item'] = item
+    rvalue['form'] = item_form.render(page=get_current_form_page(clazz, request))
+    return rvalue
 
 ###########################################################################
 #                               REST SERVICE                              #
