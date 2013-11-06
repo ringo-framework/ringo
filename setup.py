4fe139d31a9386055ec9d17532dcba236fff78ab
Torsten Irländer <torsten.irlaender@intevation.de>
2013-11-06 19:15:35 +0100

    Added Changes and set version info to 0.7.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requires = [
 ]
 
 setup(name='ringo',
-      version='0.6.4',
+      version='0.7.0',
       description='A simple web framework with base functionality to build web applications.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
