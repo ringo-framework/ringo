8791bf62d20fec76259e0ec5907098eda939cfb4
Torsten Irländer <torsten@irlaender.de>
2013-11-22 23:51:08 +0100

    Worked on statemachine and implemented a mechanism to check if a transition is available.

--- ringo/model/statemachine.py
+++ ringo/model/statemachine.py
@@ -18,26 +18,28 @@ def walk(state, found=[]):
 
 
 def null_handler(item):
-    pass
+    return item
+
+
+def null_condition(item):
+    return True
 
 
 class Statemachine(object):
     """Statemachine class"""
 
-    def __init__(self, item, state=None):
-        """Initialise the statemachine for the given item and provide
-        the start state of the machine and optionally the current state.
-        If the current state is not set then the start state is set as
-        current state."""
+    def __init__(self, item):
+        """Initialise the statemachine for the given item."""
         self._item = item
         self._root = self.setup()
         self._current = self._root
 
-        if state is not None:
-            for st in self.get_states():
-                if st._id == int(state):
-                    self._current = st
-                    break
+        # Try to set the current state of the statemaching by getting
+        # the current state from the item.
+        for st in self.get_states():
+            if st._id == self._item.state:
+                self._current = st
+                break
 
     def setup(self):
         """Need to be implemented"""
@@ -58,7 +60,8 @@ class Statemachine(object):
 
         """
         for transition in self._current.get_transitions():
-            if transition._end_state._id == state:
+            if (transition._end_state._id == state or
+               transition._end_state == state):
                 transition._handler(self._item)
                 self._current = transition._end_state
                 return self._current
@@ -75,18 +78,21 @@ class Statemachine(object):
 class Transition(object):
     """A class for transitions between to states"""
 
-    def __init__(self, start_state, end_state, label=None, handler=None):
+    def __init__(self, start_state, end_state,
+                 label=None, handler=None, condition=None):
         """Creates a transition between to states.
 
         :start_state: @todo
         :end_state: @todo
         :handler: @todo
+        :condition: @todo
 
         """
         self._label = label
         self._start_state = start_state
         self._end_state = end_state
         self._handler = handler
+        self._condition = condition
 
     def get_start(self):
         """Returns the start state of the transition
@@ -115,27 +121,47 @@ class Transition(object):
     def exchange(self):
         """Exchanges the start state to the end state.
         :returns: End state of the transtion.
-
         """
-        return self.get_end()
+        if self.is_available():
+            state = self.get_end()
+            self._handler(state._statemachine._item)
+            return state
+        else:
+            raise Exception("Change of state failed")
+
+    def is_available(self):
+        """Check is the transition is available for the item in the
+        statemachine. The function will call the condition function with
+        the item of the statemachine. It returns true if the state is
+        available (check succeeds) and returns False if the conditions
+        for a state change are not met."""
+        if self._condition:
+            return self._condition(self.get_start()._statemachine._item)
+        else:
+            return True
 
 
 class State(object):
     """Docstring for State """
 
-    def __init__(self, id, label):
+    def __init__(self, statemachine, id, label, description=None):
         """@todo: to be defined
 
+        :statemachine: @todo
         :id: @todo
         :label: @todo
+        :description: @todo
 
         """
+        self._statemachine = statemachine
         self._id = id
         self._label = label
+        self._description = description
         self._transitions = []
 
-    def add_transition(self, to_state, label=None, handler=None):
-        trans = Transition(self, to_state, label, handler)
+    def add_transition(self, to_state, label=None,
+                       handler=None, condition=None):
+        trans = Transition(self, to_state, label, handler, condition)
         self._transitions.append(trans)
 
     def get_transitions(self):
@@ -143,4 +169,9 @@ class State(object):
         :returns:  List of Transitions.
 
         """
-        return self._transitions
+        # Check if the
+        transitions = []
+        for trans in self._transitions:
+            if trans.is_available():
+                transitions.append(trans)
+        return transitions
--- ringo/unittests/test_statemachine.py
+++ ringo/unittests/test_statemachine.py
@@ -3,39 +3,48 @@
 
 import unittest
 from ringo.model.statemachine import Statemachine, State, \
-    null_handler as handler
+    null_handler as handler, null_condition as condition
 
+
+def testhandler(item):
+    item.testdata = "bar"
+    return item
+
+def testcondition(item):
+    return False
 
 class DummyItem(object):
-    pass
+    def __init__(self, state=1):
+        self.state = state
+        self.testdata = "foo"
 
 
 class DummyStatemachine(Statemachine):
 
     def setup(self):
-        s1 = State(1, "Unconfirmed")
-        s2 = State(2, "New")
-        s3 = State(3, "Reopend")
-        s4 = State(4, "Assigned")
-        s5 = State(5, "Resolved")
-        s6 = State(6, "Verfified")
-        s7 = State(7, "Closed")
+        s1 = State(self, 1, "Unconfirmed")
+        s2 = State(self, 2, "New")
+        s3 = State(self, 3, "Reopend")
+        s4 = State(self, 4, "Assigned")
+        s5 = State(self, 5, "Resolved")
+        s6 = State(self, 6, "Verfified")
+        s7 = State(self, 7, "Closed")
 
-        s1.add_transition(s5, "Decline", handler)
-        s1.add_transition(s2, "Accept", handler)
+        s1.add_transition(s5, "Decline", handler, condition)
+        s1.add_transition(s2, "Accept", handler, condition)
 
-        s2.add_transition(s4, "Assign", handler)
-        s3.add_transition(s4, "Reassign", handler)
+        s2.add_transition(s4, "Assign", handler, condition)
+        s3.add_transition(s4, "Reassign", handler, condition)
 
-        s4.add_transition(s5, "Resolve", handler)
+        s4.add_transition(s5, "Resolve", handler, condition)
 
-        s5.add_transition(s4, "Revise", handler)
-        s5.add_transition(s6, "Verify", handler)
+        s5.add_transition(s4, "Revise", handler, condition)
+        s5.add_transition(s6, "Verify", handler, condition)
 
-        s6.add_transition(s7, "Close", handler)
-        s6.add_transition(s5, "Reopen", handler)
+        s6.add_transition(s7, "Close", testhandler, condition)
+        s6.add_transition(s5, "Reopen", handler, testcondition)
 
-        s7.add_transition(s3, "Reopen", handler)
+        s7.add_transition(s3, "Reopen", handler, condition)
         return s1
 
 
@@ -55,8 +64,24 @@ class TestSequenceFunctions(unittest.TestCase):
         with self.assertRaises(Exception):
             self.sm.set_state(3)
 
+    def test_s6s7(self):
+        self.sm = DummyStatemachine(DummyItem(6))
+        state = self.sm._current
+        self.assertEqual(state._id, 6)
+        transitions = state.get_transitions()
+        state = self.sm.set_state(transitions[0].exchange())
+        self.assertEqual(state._id, 7)
+        self.assertEqual(state._statemachine._item.testdata, 'bar')
+
+    def test_s6s8(self):
+        self.sm = DummyStatemachine(DummyItem(6))
+        state = self.sm._current
+        self.assertEqual(state._id, 6)
+        transitions = state.get_transitions()
+        self.assertTrue(len(transitions) == 1)
+
     def test_start_state(self):
-        self.sm = DummyStatemachine(DummyItem(), 2)
+        self.sm = DummyStatemachine(DummyItem(2))
         state = self.sm.get_state()
         self.assertEqual("New", state._label)
 
