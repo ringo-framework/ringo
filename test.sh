0cb51a897973a364d6511cbe0c774b36e3673b4f
Torsten Irländer <torsten@irlaender.de>
2013-12-03 22:37:57 +0100

    Use alembic to setup the test database.

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm test.sqlite
-initialize_ringo_db test.ini
+alembic -c alembic-test.ini upgrade head
+#initialize_ringo_db test.ini
 #nosetests
 behave ringo/tests/behave/features --logging-level=ERROR
 coverage report -m --include="ringo*" --omit="ringo/test*"
