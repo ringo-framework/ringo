d0faf315f460c5476ccd96733eb441ad0800ee00
Torsten Irländer <torsten@irlaender.de>
2014-01-02 19:07:10 +0100

    Added tags module

---
+++ ringo/model/tag.py
@@ -0,0 +1,38 @@
+import sqlalchemy as sa
+from ringo.model import Base
+from ringo.model.base import BaseItem, BaseFactory
+from ringo.model.modul import ModulItem, _create_default_actions
+from ringo.model.mixins import Owned
+
+
+class TagFactory(BaseFactory):
+
+    def create(self, user=None):
+        new_item = BaseFactory.create(self, user)
+        return new_item
+
+
+class Tag(BaseItem, Owned, Base):
+    __tablename__ = 'tags'
+    _modul_id = 12
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column('name', sa.Text, default=None)
+    description = sa.Column('description', sa.Text, default=None)
+    tagtype = sa.Column('type', sa.Integer, default=None)
+
+    def __unicode__(self):
+        return str(self.id)
+
+def init_model(dbsession):
+    """Will setup the initial model for the tag.
+
+    :dbsession: Database session to which the items will be added.
+    :returns: None
+    """
+    modul = ModulItem(name='tags')
+    modul.clazzpath = "ringo.model.tag.Tag"
+    modul.label = "Tag"
+    modul.label_plural = "Tags"
+    modul.display = "admin-menu"
+    modul.actions.extend(_create_default_actions(dbsession))
+    dbsession.add(modul)
