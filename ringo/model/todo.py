4ae6de08e522d649294c3727478dedf8b697487a
Torsten Irländer <torsten.irlaender@intevation.de>
2014-01-03 17:43:40 +0100

    Added statemachine to the todomodel

--- ringo/model/todo.py
+++ ringo/model/todo.py
@@ -2,7 +2,34 @@ import sqlalchemy as sa
 from ringo.model import Base
 from ringo.model.base import BaseItem, BaseFactory
 from ringo.model.modul import ModulItem, _create_default_actions
-from ringo.model.mixins import Owned
+from ringo.model.mixins import Owned, StateMixin
+from ringo.model.statemachine import Statemachine, State, null_handler, null_condition
+
+
+class TodoStatemachine(Statemachine):
+    def setup(self):
+        s1 = State(self, 1, "New")
+        s2 = State(self, 2, "In Progress")
+        s3 = State(self, 3, "Done")
+
+        s1.add_transition(s2, "Start", null_handler, null_condition)
+        s2.add_transition(s3, "Resolve", null_handler, null_condition)
+        s3.add_transition(s2, "Reopen", null_handler, null_condition)
+        return s1
+
+class TodoStateMixin(StateMixin):
+        _statemachines = {'todo_state_id': TodoStatemachine}
+        # Configue a field in the model which saves the current
+        # state per state machine
+        todo_state_id = sa.Column(sa.Integer, default=1)
+
+        # Optional. Create a property to access the statemachine
+        # like an attribute. This gets usefull if you want to access
+        # the state in overview lists.
+        @property
+        def todo_state(self):
+            state = self.get_statemachine('todo_state_id')
+            return state.get_state()
 
 
 class TodoFactory(BaseFactory):
@@ -12,7 +39,7 @@ class TodoFactory(BaseFactory):
         return new_item
 
 
-class Todo(BaseItem, Owned, Base):
+class Todo(BaseItem, Owned, TodoStateMixin, Base):
     __tablename__ = 'todos'
     _modul_id = 13
     id = sa.Column(sa.Integer, primary_key=True)
