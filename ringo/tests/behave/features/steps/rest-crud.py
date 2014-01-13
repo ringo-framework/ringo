d9b5b0843ef390dbb2e38b811b36e5f90c38e2bd
Torsten Irländer <torsten@irlaender.de>
2014-01-13 23:17:11 +0100

    Mark some restcalls as wip (work in progress) and ignore them on default tests

--- ringo/tests/behave/features/steps/rest-crud.py
+++ ringo/tests/behave/features/steps/rest-crud.py
@@ -7,11 +7,10 @@ def step_impl(context, modul):
     path = get_modul_path(modul)
     context.resp = context.app.get("/rest/%s" % "/".join(path), status="*")
 
-#@when(u'calls the create rest-url of modul {modul}')
-#def step_impl(context, modul):
-#    path = get_modul_path(modul)
-#    path.append("create")
-#    context.resp = context.app.get("/rest/%s" % "/".join(path), status="*")
+@when(u'calls the create rest-url of modul {modul}')
+def step_impl(context, modul):
+    path = get_modul_path(modul)
+    context.resp = context.app.post("/rest/%s" % "/".join(path), {}, status="*")
 
 @when(u'calls the read rest-url for item {id} of modul {modul}')
 def step_impl(context, id, modul):
@@ -19,28 +18,14 @@ def step_impl(context, id, modul):
     path.append(str(id))
     context.resp = context.app.get("/rest/%s" % "/".join(path), status="*")
 
-#@when(u'calls the edit rest-url for item {id} of modul {modul}')
-#def step_impl(context, id, modul):
-#    path = get_modul_path(modul)
-#    path.append("update")
-#    path.append(str(id))
-#    context.resp = context.app.get("/rest/%s" % "/".join(path), status="*")
+@when(u'calls the edit rest-url for item {id} of modul {modul}')
+def step_impl(context, id, modul):
+    path = get_modul_path(modul)
+    path.append(str(id))
+    context.resp = context.app.put("/rest/%s" % "/".join(path), {}, status="*")
 
-#@when(u'calls the delete rest-url for item {id} of modul {modul}')
-#def step_impl(context, id, modul):
-#    path = get_modul_path(modul)
-#    path.append("delete")
-#    path.append(str(id))
-#    context.resp = context.app.get("/rest/%s" % "/".join(path), status="*")
-
-#@when(u'confirms deletion for item {id} of modul {modul}')
-#def step_impl(context, id, modul):
-#    path = get_modul_path(modul)
-#    path.append("delete")
-#    path.append(str(id))
-#    csrf = get_csrf_token(context.resp)
-#    values = {
-#        "confirmed": "1",
-#        "csrf_token": csrf
-#    }
-#    context.resp = context.app.post("/%s" % "/".join(path), values)
+@when(u'calls the delete rest-url for item {id} of modul {modul}')
+def step_impl(context, id, modul):
+    path = get_modul_path(modul)
+    path.append(str(id))
+    context.resp = context.app.delete("/rest/%s" % "/".join(path), status="*")
