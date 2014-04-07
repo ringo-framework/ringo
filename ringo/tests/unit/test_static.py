120d77cbced463cfac6fbd488cef2fa2a721dddc
Torsten Irländer <torsten@irlaender.de>
2014-04-07 20:11:01 +0200

    Removed creation of DummyRequest. request is already present in Testcase

--- ringo/tests/unit/test_static.py
+++ ringo/tests/unit/test_static.py
@@ -22,20 +22,17 @@ class StaticViewTests(BaseUnitTest):
 
     def test_about_view(self):
         from ringo.views.home import about_view
-        request = testing.DummyRequest()
-        result = about_view(request)
+        result = about_view(self.request)
         self.assertEqual(result['app_title'], 'Ringo')
 
     def test_contact_view(self):
         from ringo.views.home import contact_view
-        request = testing.DummyRequest()
-        result = contact_view(request)
+        result = contact_view(self.request)
         self.assertEqual(len(result), 0)
 
     def test_version_view(self):
         from ringo.views.home import version_view
-        request = testing.DummyRequest()
-        result = version_view(request)
+        result = version_view(self.request)
         self.assertEqual(len(result), 7)
         self.assertEqual(result['app_title'], 'Ringo')
         self.assertEqual(result['app_name'], "ringo")
