4679045930a2ca3e95cf6c2456dc6a2ce2298950
Torsten Irländer <torsten@irlaender.de>
2014-04-07 20:25:18 +0200

    Moved methods into other testcase.

--- ringo/tests/unit/test_static.py
+++ ringo/tests/unit/test_static.py
@@ -11,15 +11,6 @@ class StaticAuthViewTests(BaseUnitTest):
         self.assertTrue("reminders" in result.keys())
         self.assertTrue("news" in result.keys())
 
-
-class StaticViewTests(BaseUnitTest):
-
-    def test_home_unauthenticated_view(self):
-        from ringo.views.home import index_view
-        self.request.user = None
-        result = index_view(self.request)
-        self.assertEqual(len(result), 0)
-
     def test_about_view(self):
         from ringo.views.home import about_view
         result = about_view(self.request)
@@ -36,3 +27,12 @@ class StaticViewTests(BaseUnitTest):
         self.assertEqual(len(result), 7)
         self.assertEqual(result['app_title'], 'Ringo')
         self.assertEqual(result['app_name'], "ringo")
+
+
+class StaticViewTests(BaseUnitTest):
+
+    def test_home_unauthenticated_view(self):
+        from ringo.views.home import index_view
+        self.request.user = None
+        result = index_view(self.request)
+        self.assertEqual(len(result), 0)
