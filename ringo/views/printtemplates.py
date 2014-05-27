dc4f8a927c2dd66a689801982faf341aaa058b5c
Torsten Irländer <torsten.irlaender@intevation.de>
2014-05-27 11:03:18 +0200

    Removed default views in the printtemplates modul

--- ringo/views/printtemplates.py
+++ ringo/views/printtemplates.py
@@ -1,14 +1,10 @@
 import logging
 from pyramid.view import view_config
 
-from ringo.views.base import list_, create_, update_, read_, delete_
-from ringo.views.base import list_, create_, update_, read_, delete_,export_, import_
+from ringo.views.base import create_, update_, read_
 from ringo.views.json import (
-    list_   as json_list,
     create_ as json_create,
     update_ as json_update,
-    read_   as json_read,
-    delete_ as json_delete
     )
 from ringo.views.files import save_file
 from ringo.model.printtemplate import Printtemplate
@@ -16,13 +12,6 @@ from ringo.model.printtemplate import Printtemplate
 log = logging.getLogger(__name__)
 
 #                                HTML VIEW                                #
-
-@view_config(route_name=Printtemplate.get_action_routename('list'),
-             renderer='/default/list.mako',
-             permission='list')
-def list(request):
-    return list_(Printtemplate, request)
-
 
 @view_config(route_name=Printtemplate.get_action_routename('create'),
              renderer='/default/create.mako',
@@ -38,12 +27,6 @@ def update(request):
     return update_(Printtemplate, request, callback=save_file)
 
 
-@view_config(route_name=Printtemplate.get_action_routename('read'),
-             renderer='/default/read.mako',
-             permission='read')
-def read(request):
-    return read_(Printtemplate, request)
-
 @view_config(route_name=Printtemplate.get_action_routename('download'),
              permission='download')
 def download(request):
@@ -55,35 +38,7 @@ def download(request):
     response.body = item.data
     return response
 
-@view_config(route_name=Printtemplate.get_action_routename('delete'),
-             renderer='/default/confirm.mako',
-             permission='delete')
-def delete(request):
-    return delete_(Printtemplate, request)
-
-
-@view_config(route_name=Printtemplate.get_action_routename('export'),
-             renderer='/default/export.mako',
-             permission='export')
-def export(request):
-    return export_(Printtemplate, request)
-
-
-@view_config(route_name=Printtemplate.get_action_routename('import'),
-             renderer='/default/import.mako',
-             permission='import')
-def myimport(request):
-    return import_(Printtemplate, request)
-
 #                               REST SERVICE                              #
-
-@view_config(route_name=Printtemplate.get_action_routename('list', prefix="rest"),
-             renderer='json',
-             request_method="GET",
-             permission='list'
-             )
-def rest_list(request):
-    return json_list(Printtemplate, request)
 
 @view_config(route_name=Printtemplate.get_action_routename('create', prefix="rest"),
              renderer='json',
@@ -92,23 +47,9 @@ def rest_list(request):
 def rest_create(request):
     return json_create(Printtemplate, request, callback=save_file)
 
-@view_config(route_name=Printtemplate.get_action_routename('read', prefix="rest"),
-             renderer='json',
-             request_method="GET",
-             permission='read')
-def rest_read(request):
-    return json_read(Printtemplate, request)
-
 @view_config(route_name=Printtemplate.get_action_routename('update', prefix="rest"),
              renderer='json',
              request_method="PUT",
              permission='update')
 def rest_update(request):
     return json_update(Printtemplate, request, callback=save_file)
-
-@view_config(route_name=Printtemplate.get_action_routename('delete', prefix="rest"),
-             renderer='json',
-             request_method="DELETE",
-             permission='delete')
-def rest_delete(request):
-    return json_delete(Printtemplate, request)
