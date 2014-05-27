3cd597f27abb43efb34c988f654af32aa1bf8b67
Torsten Irländer <torsten.irlaender@intevation.de>
2014-05-27 11:03:17 +0200

    Removed default views in the files modul

--- ringo/views/files.py
+++ ringo/views/files.py
@@ -3,16 +3,7 @@ import mimetypes
 from pyramid.response import FileIter
 from pyramid.view import view_config
 
-
-from ringo.views.base import list_, create_, update_, read_, delete_,\
-export_, import_
-from ringo.views.json import (
-    list_   as json_list,
-    create_ as json_create,
-    update_ as json_update,
-    read_   as json_read,
-    delete_ as json_delete
-    )
+from ringo.views.base import create_, update_, read_
 from ringo.lib.helpers import import_model
 File = import_model('ringo.model.file.File')
 
@@ -39,13 +30,6 @@ def save_file(request, item):
         pass
     return item
 
-@view_config(route_name=File.get_action_routename('list'),
-             renderer='/default/list.mako',
-             permission='list')
-def list(request):
-    return list_(File, request)
-
-
 @view_config(route_name=File.get_action_routename('create'),
              renderer='/default/create.mako',
              permission='create')
@@ -60,20 +44,6 @@ def update(request):
     return update_(File, request, callback=save_file)
 
 
-@view_config(route_name=File.get_action_routename('read'),
-             renderer='/default/read.mako',
-             permission='read')
-def read(request):
-    return read_(File, request)
-
-
-@view_config(route_name=File.get_action_routename('delete'),
-             renderer='/default/confirm.mako',
-             permission='delete')
-def delete(request):
-    return delete_(File, request)
-
-
 @view_config(route_name=File.get_action_routename('download'),
              permission='download')
 def download(request):
@@ -84,53 +54,3 @@ def download(request):
     response.content_disposition = 'attachment; filename=%s' % item.name
     response.body = item.data
     return response
-
-@view_config(route_name=File.get_action_routename('export'),
-             renderer='/default/export.mako',
-             permission='export')
-def export(request):
-    return export_(File, request)
-
-@view_config(route_name=File.get_action_routename('import'),
-             renderer='/default/import.mako',
-             permission='import')
-def myimport(request):
-    return import_(File, request)
-
-#                               REST SERVICE                              #
-
-@view_config(route_name=File.get_action_routename('list', prefix="rest"),
-             renderer='json',
-             request_method="GET",
-             permission='list'
-             )
-def rest_list(request):
-    return json_list(File, request)
-
-@view_config(route_name=File.get_action_routename('create', prefix="rest"),
-             renderer='json',
-             request_method="POST",
-             permission='create')
-def rest_create(request):
-    return json_create(File, request, encrypt_password)
-
-@view_config(route_name=File.get_action_routename('read', prefix="rest"),
-             renderer='json',
-             request_method="GET",
-             permission='read')
-def rest_read(request):
-    return json_read(File, request)
-
-@view_config(route_name=File.get_action_routename('update', prefix="rest"),
-             renderer='json',
-             request_method="PUT",
-             permission='update')
-def rest_update(request):
-    return json_update(File, request)
-
-@view_config(route_name=File.get_action_routename('delete', prefix="rest"),
-             renderer='json',
-             request_method="DELETE",
-             permission='delete')
-def rest_delete(request):
-    return json_delete(File, request)
