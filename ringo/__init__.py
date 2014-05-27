9895ad36a4d54ded5cff7c64740bf4328a4be255
Torsten Irländer <torsten.irlaender@intevation.de>
2014-05-27 11:02:55 +0200

    Imperative configuration of base LCRUD views

--- ringo/__init__.py
+++ ringo/__init__.py
@@ -135,14 +135,53 @@ def setup_modul(config, clazz):
     :returns: config
 
     """
+    from ringo.views.base import action_view_mapping as wavm
+    from ringo.views.json import action_view_mapping as ravm
     name = clazz.__tablename__
-    #  Setup web interface
     for action in clazz.get_item_actions():
-        route_name = "%s-%s" % (name, action.name.lower())
+        action_name = action.name.lower()
+        #  Setup web route and views
+        route_name = clazz.get_action_routename(action.name.lower())
         route_url = "%s/%s" % (name, action.url)
-        log.debug("Adding route: %s, %s" % (route_name, route_url))
+        log.debug("Adding WEB route: %s, %s" % (route_name, route_url))
         config.add_route(route_name, route_url,
                          factory=get_resource_factory(clazz))
+        view_func = wavm.get(action_name)
+        if view_func:
+            if action_name == "delete":
+                template = "confirm"
+            else:
+                template = action_name
+            config.add_view(view_func,
+                            route_name=route_name,
+                            renderer='/default/%s.mako' % template,
+                            permission=action.permission or action_name)
+
+        #  Setup REST route and views
+        action_method_mapping = {
+            "list": "GET",
+            "create": "POST",
+            "read": "GET",
+            "update": "PUT",
+            "delete": "DELETE"
+        }
+        route_name = clazz.get_action_routename(action.name.lower(),
+                                                prefix="rest")
+        tmpurl = action.url.split("/")
+        if len(tmpurl) > 1:
+            route_url = "rest/%s/%s" % (name, tmpurl[1])
+        else:
+            route_url = "rest/%s" % (name)
+        log.debug("Adding REST route: %s, %s" % (route_name, route_url))
+        config.add_route(route_name, route_url,
+                         factory=get_resource_factory(clazz))
+        view_func = ravm.get(action_name)
+        if view_func:
+            config.add_view(view_func,
+                            route_name=route_name,
+                            renderer='json',
+                            request_method=action_method_mapping[action_name],
+                            permission=action.permission or action_name)
 
         # Add bundle action.
         if action.name == "List":
@@ -156,9 +195,6 @@ def setup_modul(config, clazz):
             config.add_view(bundle_, route_name=route_name,
                             renderer='/default/bundle.mako',
                             permission='list')
-
-    #  Setup REST interface
-    add_rest_service(config, clazz)
 
 
 def main(global_config, **settings):
