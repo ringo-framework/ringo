aaaeb2cd5234a40ed6d759646499bce58a4641ad
Torsten Irländer <torsten@irlaender.de>
2014-06-11 12:48:48 +0200

    Dynamically import all models.

--- ringo/scaffolds/basic/+package+/model/__init__.py
+++ ringo/scaffolds/basic/+package+/model/__init__.py
@@ -1,1 +1,12 @@
+import os
 from ringo.model import Base
+from ringo.lib import helpers
+
+# Dynamically import all model files here to have the model available in
+# the sqlalchemy metadata. This is needed for the schema migration with
+# alembic. Otherwise the migration will produce many table drops as the
+# tables of the models are not present in the metadata
+model_dir = os.path.dirname(os.path.realpath(__file__))
+modul_files = [ f for f in os.listdir(model_dir) if f.split(".")[-1] == "py" ]
+for model in modul_files:
+    helpers.dynamic_import(__name__ + "." + model.split(".")[0])
