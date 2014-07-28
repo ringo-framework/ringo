5c8882b2f68cede3c7de7e76d173b4c089e0bf55
Torsten Irländer <torsten@irlaender.de>
2014-07-28 21:15:26 +0200

    Added SQL script to fix the postgresql sequences after initialising the database.

---
+++ ringo/scripts/generate_fix_sequence_stmnts.sql
@@ -0,0 +1,24 @@
+-- https://wiki.postgresql.org/wiki/Fixing_Sequences
+-- Used to fix nextval of sequences after creating the database in a postgresql.
+--
+-- Usage:
+-- ------
+-- psql -Atq -f generate_fix_sequence_stmnts.sql -o temp dbname
+-- psql -f temp dbname
+-- rm temp
+SELECT 'SELECT SETVAL(' ||
+       quote_literal(quote_ident(PGT.schemaname) || '.' || quote_ident(S.relname)) ||
+       ', COALESCE(MAX(' ||quote_ident(C.attname)|| ')) ) FROM ' ||
+       quote_ident(PGT.schemaname)|| '.'||quote_ident(T.relname)|| ';'
+FROM pg_class AS S,
+     pg_depend AS D,
+     pg_class AS T,
+     pg_attribute AS C,
+     pg_tables AS PGT
+WHERE S.relkind = 'S'
+    AND S.oid = D.objid
+    AND D.refobjid = T.oid
+    AND D.refobjid = C.attrelid
+    AND D.refobjsubid = C.attnum
+    AND T.relname = PGT.tablename
+ORDER BY S.relname;
