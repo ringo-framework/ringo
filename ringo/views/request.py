f2072c9eb98605d8542b85ef346acb88891d6e1b
Torsten Irländer <torsten.irlaender@intevation.de>
2014-08-15 21:33:52 +0200

    Moved handle_request and get_current_form_page into lib.request

--- ringo/views/request.py
+++ ringo/views/request.py
@@ -2,6 +2,63 @@
 from pyramid.httpexceptions import HTTPBadRequest
 
 
+def handle_params(clazz, request):
+    """Handles varios sytem GET params comming with the request
+    Known params are:
+
+     * backurl: A url which should be called instead of the default
+       action after the next request succeeds. The backurl will be saved
+       in the session and stays there until it is deleted on a
+       successfull request. So take care to delete it to not mess up
+       with the application logic.
+     * form: The name of a alternative form configuration which is
+       used for the request.
+     * values: A comma separated list of key/value pair. Key and value
+       are separated with an ":"
+     * addrelation: A ":" separated string 'relation:class:id' to identify that
+       a item with id should be linked to the relation of this item.
+    """
+    params = {}
+    backurl = request.GET.get('backurl')
+    if backurl:
+        request.session['%s.backurl' % clazz] = backurl
+        params['backurl'] = backurl
+    values = request.GET.get('values')
+    if values:
+        params['values'] = {}
+        for kvpair in values.split(','):
+            key, value = kvpair.split(':')
+            params['values'][key] = value
+    form = request.GET.get('form')
+    if form:
+        request.session['%s.form' % clazz] = form
+        params['form'] = form
+    relation = request.GET.get('addrelation')
+    if relation:
+        request.session['%s.addrelation' % clazz] = relation
+        params['addrelation'] = relation
+    request.session.save()
+    return params
+
+
+def get_current_form_page(clazz, request):
+    """Returns the id of the currently selected page. The currently
+    selected page is saved in the session. If there is no saved value
+    then the the first page is returned
+
+    :clazz: The clazz for which the form is displayed
+    :request: Current request
+    :returns: id of the currently selected page. Default: 1
+    """
+    itemid = request.matchdict.get('id')
+    item = clazz.__tablename__
+    page = request.session.get('%s.%s.form.page' % (item, itemid))
+    if page:
+        return int(page)
+    else:
+        return 1
+
+
 def get_item_from_request(request):
     """On every request pyramid will use a resource factory to load the
     requested resource for the current request. This resource is the
