1da084e1fe954ff5ffd0b2014bbeb75493feca10
Torsten Irländer <torsten.irlaender@intevation.de>
2014-08-20 09:40:34 +0200

    Refactored delete view

--- ringo/views/base/delete.py
+++ ringo/views/base/delete.py
@@ -13,14 +13,33 @@ from ringo.views.request import (
 log = logging.getLogger(__name__)
 
 
-def _handle_delete_request(clazz, request, items):
+def _handle_redirect(request):
+    """Will return a redirect.
+
+    :request: Current request
+    :returns: Redirect
+
+    """
+    clazz = request.context.__model__
+    backurl = request.session.get('%s.backurl' % clazz)
+    if backurl:
+        # Redirect to the configured backurl.
+        del request.session['%s.backurl' % clazz]
+        request.session.save()
+        return HTTPFound(location=backurl)
+    else:
+        # Redirect to the list view.
+        route_name = clazz.get_action_routename('list')
+        url = request.route_path(route_name)
+        return HTTPFound(location=url)
+
+
+def _handle_delete_request(request, items):
+    clazz = request.context.__model__
     _ = request.translate
-    rvalue = {}
     if request.method == 'POST' and is_confirmed(request):
         for item in items:
             request.db.delete(item)
-        route_name = clazz.get_action_routename('list')
-        url = request.route_path(route_name)
         item_label = clazz.get_item_modul(request).get_label(plural=True)
         mapping = {'item_type': item_label, 'num': len(items)}
         msg = _('Deleted ${num} ${item_type} successfull.', mapping=mapping)
@@ -29,19 +48,10 @@ def _handle_delete_request(clazz, request, items):
         # Invalidate cache
         invalidate_cache()
         # Handle redirect after success.
-        backurl = request.session.get('%s.backurl' % clazz)
-        if backurl:
-            # Redirect to the configured backurl.
-            del request.session['%s.backurl' % clazz]
-            request.session.save()
-            return HTTPFound(location=backurl)
-        else:
-            # Redirect to the update view.
-            return HTTPFound(location=url)
+        return _handle_redirect(request)
     else:
-        # FIXME: Get the ActionItem here and provide this in the Dialog to get
-        # the translation working (torsten) <2013-07-10 09:32>
         renderer = ConfirmDialogRenderer(request, clazz, 'delete')
+        rvalue = {}
         rvalue['dialog'] = renderer.render(items)
         rvalue['clazz'] = clazz
         rvalue['item'] = items
@@ -49,11 +59,10 @@ def _handle_delete_request(clazz, request, items):
 
 
 def delete(request):
-    clazz = request.context.__model__
     item = get_item_from_request(request)
     handle_history(request)
     handle_params(request)
-    return _handle_delete_request(clazz, request, [item])
+    return _handle_delete_request(request, [item])
 
 
 def rest_delete(request):
