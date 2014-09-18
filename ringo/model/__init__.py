21e6531d42b6c4b7af44813d59285c2217cfb22b
Torsten Irländer <torsten.irlaender@intevation.de>
2014-09-18 16:16:26 +0200

    Added global list to register extensions in ringo. Changed code to import all models to include models from the extensions.

--- ringo/model/__init__.py
+++ ringo/model/__init__.py
@@ -2,15 +2,22 @@ import os
 from sqlalchemy.ext.declarative import declarative_base
 from ringo.lib import helpers
 
-# global declarative base class.
+# Define enabled ringo extenstion here. Extensions will be initialised
+# on application startup.
+extensions = [
+]
+
+# Global declarative base class.
 Base = declarative_base()
 
-
-# Dynamically import all model files here to have the model available in
-# the sqlalchemy metadata. This is needed for the schema migration with
-# alembic. Otherwise the migration will produce many table drops as the
-# tables of the models are not present in the metadata
+# Dynamically import all model files and extensions here to have the
+# model available in the sqlalchemy metadata. This is needed for the
+# schema migration with alembic. Otherwise the migration will produce
+# many table drops as the tables of the models are not present in the
+# metadata
 model_dir = os.path.dirname(os.path.realpath(__file__))
-modul_files = [ f for f in os.listdir(model_dir) if f.split(".")[-1] == "py" ]
+modul_files = [f for f in os.listdir(model_dir) if f.split(".")[-1] == "py"]
 for model in modul_files:
     helpers.dynamic_import(__name__ + "." + model.split(".")[0])
+for extension in extensions:
+    helpers.dynamic_import(extension + ".model")
