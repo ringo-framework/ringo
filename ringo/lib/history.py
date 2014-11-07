d79be156969bd849a4853e5518c6f79662c73ae6
Torsten Irländer <torsten@irlaender.de>
2014-11-08 00:50:40 +0100

    Handle case when history is empty

--- ringo/lib/history.py
+++ ringo/lib/history.py
@@ -7,7 +7,7 @@ class History:
         """Adds an url to the history if the url is not already the most
         recent entry. If there are more than 10 entries in the list the
         oldes entry will be removed."""
-        if url != self.history[-1]:
+        if not self.history or url != self.history[-1]:
             self.history.append(url)
         if len(self.history) > 10:
             del self.history[0]
