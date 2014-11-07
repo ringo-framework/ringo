c01c6417fb7393b3c40d2e6a466ddfacce9d52b8
Torsten Irländer <torsten@irlaender.de>
2014-11-08 00:40:33 +0100

    Modified push method of the history. Only add a url if it is not already the last recent entry. Delete  the oldest entry if the history length becomes longer tha 10

--- ringo/lib/history.py
+++ ringo/lib/history.py
@@ -4,7 +4,14 @@ class History:
         self.history = history
 
     def push(self, url):
-        self.history.append(url)
+        """Adds an url to the history if the url is not already the most
+        recent entry. If there are more than 10 entries in the list the
+        oldes entry will be removed."""
+        if url != self.history[-1]:
+            self.history.append(url)
+        if len(self.history) > 10:
+            del self.history[0]
+        print self.history
 
     def pop(self):
         if len(self.history) > 0:
