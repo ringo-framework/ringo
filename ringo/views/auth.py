52f9a217a674c65fea91543e9ef41b9b318018d0
Torsten Irländer <torsten.irlaender@intevation.de>
2014-11-20 11:33:36 +0100

    Fixed translation for the login forms.

--- ringo/views/auth.py
+++ ringo/views/auth.py
@@ -46,7 +46,8 @@ def login(request):
     settings = request.registry.settings
     config = Config(load(get_path_to_form_config('auth.xml', 'ringo')))
     form_config = config.get_form('loginform')
-    form = Form(form_config, csrf_token=request.session.get_csrf_token())
+    form = Form(form_config, csrf_token=request.session.get_csrf_token(),
+                translate=_)
     if request.POST:
         form.validate(request.params)
         username = form.data.get('login')
@@ -93,7 +94,8 @@ def register_user(request):
     _ = request.translate
     config = Config(load(get_path_to_form_config('auth.xml', 'ringo')))
     form_config = config.get_form('register_user')
-    form = Form(form_config, csrf_token=request.session.get_csrf_token())
+    form = Form(form_config, csrf_token=request.session.get_csrf_token(),
+                translate=_)
     # Do extra validation which is not handled by formbar.
     # Is the login unique?
     validator = Validator('login',
@@ -181,7 +183,8 @@ def forgot_password(request):
     _ = request.translate
     config = Config(load(get_path_to_form_config('auth.xml', 'ringo')))
     form_config = config.get_form('forgot_password')
-    form = Form(form_config, csrf_token=request.session.get_csrf_token())
+    form = Form(form_config, csrf_token=request.session.get_csrf_token(),
+                translate=_)
     if request.POST:
         if form.validate(request.params):
             username = form.data.get('login')
