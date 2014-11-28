0af8e64202bc8b39a6d4ead4e431f0d17db18ad6
Torsten Irländer <torsten.irlaender@intevation.de>
2014-11-28 11:00:11 +0100

    Use Capitalized version of the package name as class name for the extensions.

--- ringo/scaffolds/__init__.py
+++ ringo/scaffolds/__init__.py
@@ -8,3 +8,7 @@ class BasicRingoTemplate(PyramidTemplate):
 class RingoExtensionTemplate(PyramidTemplate):
     _template_dir = 'extension'
     summary = 'Ringo extension'
+
+    def pre(self, command, output_dir, vars):
+        vars['Package'] = vars['package'].capitalize()
+        return PyramidTemplate.pre(self, command, output_dir, vars)
