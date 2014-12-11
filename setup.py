f4ea9fa0f6ed3f8b765f2735bb8cdbcf90848c09
Torsten Irländer <torsten.irlaender@intevation.de>
2014-12-11 20:19:36 +0100

    Pinnend alembic to version <= 0.7.0 because of #77

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid',
     'SQLAlchemy',
-    'alembic',
+    'alembic<=0.7.0',
     'alembic_sqlite',
     'transaction',
     'pyramid_tm',
