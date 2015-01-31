41171f59c9c0905d708cda72d59cc4e0666cc3bf
Torsten Irländer <torsten@irlaender.de>
2015-01-31 17:01:39 +0100

    Copied  save_file helper method into printtemplates

--- ringo/views/printtemplates.py
+++ ringo/views/printtemplates.py
@@ -1,4 +1,5 @@
 import logging
+import mimetypes
 from pyramid.view import view_config
 
 from ringo.lib.helpers import get_action_routename
@@ -7,12 +8,36 @@ from ringo.views.base import (
     update, rest_update,
     read
 )
-from ringo.views.files import save_file
 from ringo.model.printtemplate import Printtemplate
 
 log = logging.getLogger(__name__)
 
+
+def save_file(request, item):
+    """Helper function which is called after the validation of the form
+    succeeds. The function will get the data from the file from the
+    request and set it in the model including size and mime type.
+    Addiotionally it will set the filename based on the uploaded file if
+    no other name is given."""
+    try:
+        #  FIXME: Use global helper method for this??? (ti) <2015-01-31
+        #  16:49>
+        # Rewind file
+        request.POST.get('file').file.seek(0)
+        data = request.POST.get('file').file.read()
+        filename = request.POST.get('file').filename
+        item.data = data
+        item.size = len(data)
+        item.mime = mimetypes.guess_type(filename)[0]
+        if not request.POST.get('name'):
+            item.name = filename
+    except AttributeError:
+        # Will be raised if the user submits no file.
+        pass
+    return item
+
 #                                HTML VIEW                                #
+
 
 @view_config(route_name=get_action_routename(Printtemplate, 'create'),
              renderer='/default/create.mako',
