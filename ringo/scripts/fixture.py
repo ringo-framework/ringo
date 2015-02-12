5cec42774d1854b8967d44c73209c6ae67341b9f
Torsten Irländer <torsten.irlaender@intevation.de>
2015-02-12 09:09:38 +0100

    Use args.app to load and save the fixtures. Default to current application but can be used to load fixtures from other apps.

--- ringo/scripts/fixture.py
+++ ringo/scripts/fixture.py
@@ -6,7 +6,7 @@ def handle_fixture_load_command(args):
     appname = get_package_name(args.config)
     db = get_db(args.config)
     print "Loading data in %s" % db
-    for fixture, modul in get_fixtures(appname):
+    for fixture, modul in get_fixtures(args.app):
         print "Loading fixture %s" % fixture
         run("%s-admin db loaddata --loadbyid --config %s %s %s" % (appname, args.config, modul, fixture))
 
@@ -15,7 +15,7 @@ def handle_fixture_save_command(args):
     appname = get_package_name(args.config)
     db = get_db(args.config)
     print "Saving data in %s" % db
-    for fixture, modul in get_fixtures(appname):
+    for fixture, modul in get_fixtures(args.app):
         print "Saving fixture %s" % fixture
         try:
             run("%s-admin db savedata --include-relations --config %s %s > %s" % (appname, args.config, modul, fixture), hide="stderr")
