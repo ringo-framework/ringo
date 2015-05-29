f1e0b62f04ea10dab41d114dd5e713d1923770e5
Roland Geider <roland.geider@intevation.de>
2015-05-29 11:58:39 +0200

    Set x_frame_options-header to DENY
    See waskiq/issue307

--- ringo/tweens/clickjacking.py
+++ ringo/tweens/clickjacking.py
@@ -3,7 +3,7 @@ def clickjacking_factory(handler, registry):
         settings, response = registry.settings, request.response
         HEADER = 'X-Frame-Options'
         if not response.headers.get(HEADER, None):
-            option = settings.get('x_frame_options', 'SAMEORIGIN').upper()
+            option = settings.get('x_frame_options', 'DENY').upper()
             response.headers[HEADER] = option
         return handler(request)
     return clickjacking_tween
