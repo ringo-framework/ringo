88d23a4ac026f76d101796bc73378069b43e14bd
Torsten Irländer <torsten@irlaender.de>
2015-07-18 10:47:01 +0200

    Removed app.url config variable as it is not used anymore. Modified get_app_url helper method to return the path directly from the request.

--- ringo/lib/helpers/appinfo.py
+++ ringo/lib/helpers/appinfo.py
@@ -43,21 +43,23 @@ def get_app_location(name=None):
 
 
 def get_app_url(request):
-    """Returns the base url of the application if configured. The base
-    url can be configured using the `app.url` config variable in the ini
-    file. The configuration defaults to having no explicit base url. If
-    the `app.url` variable is emtpy we will determine the base url from
-    request.application_url variable. Else the value of `app.url` is
-    used"""
-    settings = request.registry.settings
-    app_url = settings.get("app.url")
-    if app_url is None:
-        # Default! No url.
-        return ""
-    elif app_url == "":
-        return request.application_url
-    else:
-        return app_url
+    """Returns the path of the application under which the application
+    is hosted on the server.
+
+    .. note::
+        This function is a helper function. It is only used to build
+        correct URLs for client sided AJAX requests in case the
+        application is hosted in a subpath.
+
+    Example:
+
+    If the application is hosted on "http://localhost:6543/foo" the
+    function will return "/foo". If it is hosted under the root
+    directory "/" is returned."""
+    path = request.application_url.strip(request.host_url)
+    if path:
+        return "/"+path
+    return "/"
 
 
 def get_app_title():
