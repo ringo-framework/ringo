544da00c5d071dc862f8c7528135c05feec04ea8
Torsten Irländer <torsten.irlaender@intevation.de>
2015-07-29 17:28:56 +0200

    Implemented logging of changes of the uid or gid

--- ringo/views/base/ownership.py
+++ ringo/views/base/ownership.py
@@ -7,6 +7,8 @@ from ringo.views.request import (
     handle_params,
     handle_history,
     get_return_value,
+    get_item_from_request,
+    get_item_modul,
     handle_POST_request,
     handle_redirect_on_success
 )
@@ -23,11 +25,29 @@ def ownership(request, callback=None, renderers=None):
     """
     handle_history(request)
     handle_params(request)
+    clazz = request.context.__model__
+    item_label = get_item_modul(request, clazz).get_label()
     form = get_ownership_form(request)
     if request.POST:
-         if handle_POST_request(form, request, callback, 'update', renderers):
-             return handle_redirect_on_success(request)
+        item = get_item_from_request(request)
+        old_gid = str(item.gid)
+        old_uid = str(item.uid)
+        new_gid = str(request.params['group'])
+        new_uid = str(request.params['owner'])
+        if handle_POST_request(form, request, callback, 'update', renderers):
+            if old_uid != new_uid:
+                log_msg = u'User {user.login} changed uid of {item_label} '\
+                          '{item.id} form {old} -> {new}'\
+                          .format(item_label=item_label, item=item, 
+                                  user=request.user, old=old_uid, new=new_uid)
+                log.info(log_msg)
+            if old_gid != new_gid:
+                log_msg = u'User {user.login} changed gid of {item_label} '\
+                          '{item.id} form {old} -> {new}'\
+                          .format(item_label=item_label, item=item, 
+                                  user=request.user, old=old_gid, new=new_gid)
+                log.info(log_msg)
+            return handle_redirect_on_success(request)
     rvalues = get_return_value(request)
-    values = {'_roles': [str(r.name) for r in request.user.roles]}
     rvalues['form'] = get_rendered_ownership_form(request)
     return rvalues
