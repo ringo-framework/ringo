f48109cc9f37515d1bd2c131a385b954375b2d3b
Roland Geider <Roland Geider roland@intevation.de>
2016-01-28 15:35:12 +0100

    Set version to 1.10.5rc1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_requires = [
 ]
 
 setup(name='ringo',
-      version = '1.10.4',
+      version = '1.10.5',
       description='A simple web framework with base functionality to build web applications.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
