77456c35f91e2d4c5bc8bdf482cc495dd32497b0
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-02-26 13:23:57 +0100

    Added testcase for changin the standing of a user

--- ringo/tests/functional/test_user.py
+++ ringo/tests/functional/test_user.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import pytest
-from pytest_ringo import login, transaction_begin, transaction_rollback
+from pytest_ringo import (
+    login, transaction_begin, transaction_rollback,
+    search_data
+)
 
 def create_user(app, login):
     """Will create a new user with the given loginname.
@@ -172,4 +175,32 @@ class TestChangeOwnPassword:
                   "password": "123123123",
                   "_retype_password": "123123123"}
         result = app.post("/users/changepassword/1", params=values, status=200)
+        transaction_rollback(app)
+
+
+@pytest.mark.incremental
+class TestSetStandin:
+
+    def test_create(self, app):
+        login(app, "admin", "secret")
+        transaction_begin(app)
+        create_user(app, "test")
+
+        # Issue1201 in Intevation waskiq tracker. If the login is
+        # changed calling the setstandin page will failed for admin users
+        # Reason is that the code tries to get the name of the user for
+        # current usergroup by mathing groupname and loginname.
+        user = search_data(app, "users", "login", "test")
+        user["login"] = "test123"
+        app.post("/users/update/%s" % user["id"], params=user, status=302)
+
+    def test_setstandin(self, app):
+        user = search_data(app, "users", "login", "test123")
+        app.get("/usergroups/setstandin/%s" % user["default_gid"])
+
+        admin = search_data(app, "users", "login", "admin")
+        app.post("/usergroups/setstandin/%s" % user["default_gid"],
+                 params={"members": [admin["id"], user["id"]]}, status=302)
+
+        app.get("/")
         transaction_rollback(app)
