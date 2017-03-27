124f6e71fa9660d197918ed218b03cedf1b6961b
Maximilian Krambach <Maximilian Krambach mkrambach@intevation.de>
2017-03-27 13:21:14 +0200

    alternative check for dirty forms
    ignoring tags with the "no-dirtyable" attribute

--- ringo/static/js/helpers.js
+++ ringo/static/js/helpers.js
@@ -19,3 +19,53 @@ function getDTLanguage(language) {
         return "default";
     }
 }
+
+/**
+ * checks if any form in the DOM is dirty, bypassing form and input tags with
+ * the attribute 'no-dirtyable'. Returns true if some change is found
+ * (e.g. < form no-dirtyable>...< /form>)
+ * (e.g. < input type='checkbox' no-dirtyable />)
+ */
+function checkDirtyForms () {
+    var forms = document.getElementsByTagName('FORM');
+    forms.forEach( function(form) {
+        if !form.attr("no-dirtyable") {
+            var childnodes = form.childNodes;
+            childnodes.forEach( function(node) {
+                if (node.tagName === 'INPUT') {
+                    if (!node.attr("no-dirtyable")) {
+                        switch type {
+                            case "checkbox":
+                            case "radio":
+                                if (node.checked != node.defaultChecked) {
+                                    return true;
+                                }
+                                break;
+                            default:
+                                //TODO check if all other input types are
+                                // covered (even html5 ones)
+                                if (node.value != node.defaultValue){
+                                    return true;
+                                }
+                        }
+                    }
+                }
+                else if (node.tagName === 'TEXTAREA') {
+                    if (!node.attr("no-dirtyable")) {
+                        if (node.value != node.defaultValue){
+                            return true;
+                        }
+                    }
+                }
+                else if (node.tagName === 'SELECT') {
+                    if (!node.attr("no-dirtyable")) {
+                        if (node.options[node.selectedIndex].defaultSelected){
+                            return true;
+                        }
+                    }
+                }
+            });
+        }
+    });
+    return false;
+})
