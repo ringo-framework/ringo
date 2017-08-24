542295f6976ee0708ff976e201b47935df0e0abc
Tom Gottfried <Tom Gottfried tom@intevation.de>
2017-08-24 12:30:19 +0200

    Unify loading of modul by name.

--- ringo/lib/helpers/misc.py
+++ ringo/lib/helpers/misc.py
@@ -2,6 +2,7 @@ import logging
 import re
 import string
 import base64
+import sqlalchemy as sa
 from datetime import datetime
 from pyramid.threadlocal import get_current_request, get_current_registry
 import formbar.converters as converters
@@ -209,6 +210,19 @@ def dynamic_import(cl):
     classname = cl[d + 1:len(cl)]
     m = __import__(cl[0:d], globals(), locals(), [classname])
     return getattr(m, classname)
+
+
+def get_modul_by_name(modulname, session=DBSession):
+    # FIXME:
+    # Compatibilty mode. Older versions of Ringo added a 's' to the
+    # extensions modul name as Ringo usally uses the plural form.
+    # Newer versions use the configured extension name. So there
+    # might be a mixture of old and new modul names in the database.
+    # This code will handle this. (ti) <2016-01-04 13:50>
+    from ringo.model.modul import ModulItem
+    return session.query(ModulItem).filter(
+        sa.or_(ModulItem.name == modulname,
+               ModulItem.name == modulname + 's')).scalar()
 
 
 def import_model(clazzpath):
